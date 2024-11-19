5b0fbcd82864a43eb957b3f2165df141d2bd57f6
tiken14 <tiken14@live.com>
2024-11-19 18:03:25 +0500

    Сэмпл чувствительных данных, миграции, урлы. CRUD для Users через Viewset

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("learns.urls"), name='learns'),
-    path('users/', include("users.urls"), name='users'),
+    path('', include("users.urls"), name='users'),
 
 ]

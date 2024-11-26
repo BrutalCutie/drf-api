ab86b02f54796d58fc99983dd3637f49c0eb15cb
tiken14 <tiken14@live.com>
2024-11-26 13:25:05 +0500

    Смена LessonAPIView на ViewSet

--- learns/urls.py
+++ learns/urls.py
@@ -1,20 +1,21 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from learns.apps import LearnsConfig
-from learns import views
-from learns.views import CourseViewSet
+from learns.views import CourseViewSet, LessonViewSet
 
 app_name = LearnsConfig.name
 
 router = DefaultRouter()
-router.register(r'course', CourseViewSet, basename='course')
+router.register(r'courses', CourseViewSet, basename='course')
+router.register(r'lessons', LessonViewSet, basename='lesson')
+
 
 urlpatterns = [
-    path('lesson/list/', views.LessonListApiView.as_view(), name='lesson-list'),
-    path('lesson/create/', views.LessonCreateApiView.as_view(), name='lesson-create'),
-    path('lesson/detail/<int:pk>/', views.LessonRetrieveApiView.as_view(), name='lesson-detail'),
-    path('lesson/update/<int:pk>/', views.LessonUpdateApiView.as_view(), name='lesson-update'),
-    path('lesson/delete/<int:pk>/', views.LessonDeleteApiView.as_view(), name='lesson-delete'),
+
+    # path('lesson/list/', views.LessonListApiView.as_view(), name='lesson-list'),
+    # path('lesson/create/', views.LessonCreateApiView.as_view(), name='lesson-create'),
+    # path('lesson/detail/<int:pk>/', views.LessonRetrieveApiView.as_view(), name='lesson-detail'),
+    # path('lesson/update/<int:pk>/', views.LessonUpdateApiView.as_view(), name='lesson-update'),
+    # path('lesson/delete/<int:pk>/', views.LessonDeleteApiView.as_view(), name='lesson-delete'),
 
 ] + router.urls
--- learns/views.py
+++ learns/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
 
 from learns.models import Course, Lesson
 from learns.serializers import CourseSerializer, LessonSerializer
@@ -9,28 +9,30 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
 
 
-class LessonListApiView(generics.ListAPIView):
+class LessonViewSet(viewsets.ModelViewSet):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 
-class LessonCreateApiView(generics.CreateAPIView):
-    serializer_class = LessonSerializer
-
-
-class LessonRetrieveApiView(generics.RetrieveAPIView):
-    serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-
-
-class LessonUpdateApiView(generics.UpdateAPIView):
-    serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-
-
-class LessonDeleteApiView(generics.DestroyAPIView):
-    serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-
-
-
+# class LessonListApiView(generics.ListAPIView):
+#     serializer_class = LessonSerializer
+#     queryset = Lesson.objects.all()
+#
+#
+# class LessonCreateApiView(generics.CreateAPIView):
+#     serializer_class = LessonSerializer
+#
+#
+# class LessonRetrieveApiView(generics.RetrieveAPIView):
+#     serializer_class = LessonSerializer
+#     queryset = Lesson.objects.all()
+#
+#
+# class LessonUpdateApiView(generics.UpdateAPIView):
+#     serializer_class = LessonSerializer
+#     queryset = Lesson.objects.all()
+#
+#
+# class LessonDeleteApiView(generics.DestroyAPIView):
+#     serializer_class = LessonSerializer
+#     queryset = Lesson.objects.all()
